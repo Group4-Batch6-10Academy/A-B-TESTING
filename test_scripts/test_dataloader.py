5e92cef5eb939cf587cb08df68c954952279e60e
Nebil Arega <36981187+nebilarega@users.noreply.github.com>
2022-09-03 12:33:44 +0300

    Changed ../data to data/ in test_dataloader.py

--- test_scripts/test_dataloader.py
+++ test_scripts/test_dataloader.py
@@ -1,12 +1,13 @@
+from scripts.dataloader import DataLoader
 import unittest
-import sys, os
+import sys
+import os
 sys.path.append(os.path.abspath(os.path.join('..')))
 
-from scripts.dataloader import DataLoader
 
 class TestDataLoader(unittest.TestCase):
     def setUp(self):
-        self.dir_name = "../data"
+        self.dir_name = "data/"
 
     def test_read_csv(self):
         """Test the readcsv method"""
@@ -15,6 +16,7 @@ class TestDataLoader(unittest.TestCase):
         pd = dataloader.read_data(self.dir_name, filename)
         col1 = pd.columns[0]
         self.assertEqual(col1, "auction_id")
+
 
 if __name__ == '__main__':
     unittest.main()
